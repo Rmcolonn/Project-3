516b5963c8da6d596d15408366a9afb5816fedab
rmcolonn <rebekah.colonnese@outlook.com>
2020-07-26 11:42:31 -0400

    changes

---
+++ app/server.r
@@ -0,0 +1,140 @@
+server <- function(input, output, session) {
+  projectData <- read_csv("Project 3 Data.csv")
+  pitcherData <- reactive({projectData %>% filter(Pitcher == input$pitcher)})
+  teamData <- reactive({projectData %>% filter(PitcherTeam == input$team)})
+  
+  observe({
+    updateSelectizeInput(session, "pitcher", 
+                         choices = as.factor(teamData()$Pitcher))
+  })
+  
+  #create plot for exploration tab
+  output$plot1a <- renderPlot({
+    ggplot(data = pitcherData(), aes( x = Date, y = SpinRate)) +
+      geom_point()
+    
+  })
+  
+  ##create table for exploration tab
+  tableData <- reactive({projectData %>% filter(Pitcher == input$pitcher & PitcherTeam == input$team) %>% 
+      dplyr::select(SpinRate,Outs, Balls, Strikes)
+                                                })
+  output$summary1 <- renderPrint({
+    (summary(tableData()))
+  })
+  
+  output$plot1b <- renderPlot({
+    ggplot(data = teamData(), aes(x = SpinRate)) +
+      geom_histogram(bins = 20) +
+      facet_wrap(~Pitcher, scales = "free") +
+      ggtitle(input$team)
+  })
+  individualGraph <- reactive({
+    ggplot(data = teamData(), aes(x = SpinRate)) +
+      geom_histogram(bins = 20) +
+      facet_wrap(~Pitcher, scales = "free") +
+      ggtitle(input$team)
+  })
+  
+  output$individualGraph <- renderPlot({
+    req(individualGraph())
+    individualGraph()
+  })
+  
+  output$Download <- downloadHandler(
+    filename = function(){
+      paste('test', '.png', sep = '')
+    },
+    content = function(file){
+      req(individualGraph())
+      ggsave(file, plot = individualGraph(), device = 'png')
+    }
+  )
+
+  ##Clustering Stuff 
+  selectedData <- reactive({
+    projectData %>% filter(Pitcher == input$pitcher3) %>% select(input$xcol, input$ycol) %>% na.omit() 
+  })
+  
+  clusters <- reactive({
+    set.seed(123)
+    kmeans(selectedData(), input$clusters, iter.max = input$iteration, algorithm = "MacQueen")
+  })
+  
+  output$plotClust <- renderPlot({
+    palette(c("red", "orange", "yellow", "green", "blue", "purple", "pink", "black", "gray"))
+    
+
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 8, cex = 5, lwd = 1)
+  })
+  
+  output$info <- renderText({
+    xy_str <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
+    }
+    xy_range_str <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1), 
+             " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
+    }
+    
+  paste0(
+    "hover: ", xy_str(input$plot_hover),
+    "brush: ", xy_range_str(input$plot_brush))
+  })
+  
+  
+  
+  ##Modeling stuff
+  #REgression
+  modelData <- reactive(projectData %>% filter(PitcherTeam == input$team2))
+  
+  lm1 <- reactive({lm(reformulate(input$IndVar, input$DepVar), data = modelData())})
+  
+  output$DepPrint <- renderPrint({input$DepVar})
+  output$IndPrint <- renderPrint({input$IndVar})
+  output$RegSum <- renderPrint({summary(lm1())})
+
+
+
+  #Classification Tree
+  knn <- reactive({caret::train(reformulate(input$IndVar2, input$DepVar2), data = modelData(), 
+                                      method = "knn" , 
+                                      trControl = trainControl(method = "repeatedcv", 
+                                                               number = as.numeric(input$number),
+                                                               repeats = as.numeric(input$repeats)), 
+                                                               preProcess = c("center", "scale"),
+                                tuneGrid = expand.grid(k = c(2:30)))
+    })
+
+  output$DepPrint2 <- renderPrint({input$DepVar2})
+  output$IndPrint2 <- renderPrint({input$IndVar2})
+  output$RegSum2 <- renderPrint({(knn())})
+
+  
+  
+  #Data tab
+  datasetInput <-  reactive({projectData %>% filter(Pitcher == input$pitcher5) %>% 
+        filter(PitcherTeam == input$team5)})
+ 
+  
+  # Table of selected dataset ----
+  output$data <- DT::renderDataTable({
+    DT::datatable(datasetInput(), options = list(scrollY = '500px'))
+  })
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file, row.names = FALSE)
+    }
+  )
+}
+
---
+++ app/ui.R
@@ -0,0 +1,152 @@
+## app.R ##
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(knitr)
+library(DT)
+
+projectData <- read_csv("Project 3 Data.csv")
+clusterData <- dplyr::select(projectData, RelSpeed:SpinAxis, RelHeight:ZoneSpeed)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Project 3"),
+  
+  ## Sidebar content
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Information", tabName = "Info", icon = icon("file-alt")),
+      menuItem("Data Exploration", tabName = "Exploration", icon = icon("th")),
+      menuItem("Clustering", tabName = "Cluster", icon = icon("th")),
+      menuItem("Modeling", tabName = "Modeling", icon = icon("th")),
+      menuItem("Data", tabName = "Data", icon = icon("th"))
+    )
+  ),
+  #Body Content
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "Info",
+              h1("App Information"), 
+              p("The purpose of this app is to....")
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "Exploration",
+              h2("Let's explore the data"),
+              selectizeInput("team", "Team", selected = "NOR_TAR",
+                             choices = levels(as.factor(projectData$PitcherTeam))),
+              selectizeInput("pitcher", "Pitcher", selected = "Bergner, Austin", 
+                             choices = levels(as.factor(projectData$Pitcher))),
+              br(),
+              
+              mainPanel( 
+                tabsetPanel(
+                  tabPanel("Pitcher Plot", plotOutput("plot1a")), 
+                  tabPanel("Summary", verbatimTextOutput("summary1")),
+                  tabPanel("Team Plot", 
+                           p("Click the download button at the bottom if you would like to donwload the plot"),
+                           plotOutput("plot1b"),
+                           fluidPage(downloadButton('Download'))))
+                
+                
+      )),
+      
+      # Third tab content
+      tabItem(tabName = "Cluster",
+              h2("K-Means Clustering"),
+              p("Select the X and Y vairables and select the pitcher you would like to see the cluster. Click and drag on the plot to get some info about the points, or hover over to see the point information."),
+              sidebarPanel(
+                selectInput('xcol', 'X Variable', names(clusterData)),
+                selectInput('ycol', 'Y Variable', names(clusterData),
+                            selected=names(clusterData)[[2]]),
+                selectizeInput("pitcher3", "Pitcher", selected = "Bergner, Austin", 
+                               choices = levels(as.factor(projectData$Pitcher))),
+                numericInput('clusters', 'Cluster count', 3,
+                             min = 1, max = 9, step = 1),
+                numericInput('iteration', '# of Iterations of Algorithm', value = 1, min = 1, max = 20, step = 1)
+              ),
+              mainPanel(
+                plotOutput('plotClust', hover = "plot_hover", brush = "plot_brush"),
+                verbatimTextOutput("info")
+              )
+      ),
+      
+      # Fourth tab content
+      tabItem(tabName = "Modeling",
+              h2("Modeling is an art...."),
+              selectizeInput("model", "Model Type", selected = "Linear Regression", 
+                             choices = c("Linear Regression", "K Nearest Neighbors")),
+              selectizeInput("team2", "Team", selected = "NOR_TAR",
+                             choices = levels(as.factor(projectData$PitcherTeam))),
+             # selectizeInput("dataSize", "Select Percentage of data for training model", selected = .9,
+              #               choices = c(.7, .75, .8, .9)),
+             # actionButton("button", "Test Model"),
+              
+      conditionalPanel(
+        condition = "input.model == 'Linear Regression'",
+        h3("Regression Analysis"), 
+        p("This data used for analysis is dependent on the team selected, and you need to select at least one independent variable to see the model"),
+        sidebarPanel(
+          p("Select the inputs for the Dependent Variable"),
+          selectInput(inputId = "DepVar", label = "Dependent Variables", choices = list("SpinRate")),
+          p("Select the inputs for the Independent Variable"),
+          selectInput(inputId = "IndVar", label = "Independent Variables", multiple = TRUE, 
+                      choices = list( "RelSpeed", "RelHeight", "RelSide", "TruePitch", "Pitcher", "BatterSide"))
+        ),
+        mainPanel(
+          verbatimTextOutput(outputId = "RegSum"),
+          verbatimTextOutput(outputId = "IndPrint"),
+          verbatimTextOutput(outputId = "DepPrint")
+        )
+      ),
+      
+      conditionalPanel(
+        condition = "input.model == 'K Nearest Neighbors'",
+        h3("K Nearest Neighbors"), 
+        p("This data used for analysis is dependent on the team selected, and you need to select at least one independent variable to see the model"),
+        sidebarPanel(
+          p("Select the inputs for the Dependent Variable"),
+          selectInput(inputId = "DepVar2", label = "Dependent Variables", choices = list("TruePitch")),
+          p("Select the inputs for the Independent Variable"),
+          selectInput(inputId = "IndVar2", label = "Independent Variables", multiple = TRUE, 
+                      choices = list( "RelSpeed", "RelHeight", "RelSide", "SpinRate", "Pitcher", "BatterSide")),
+          selectizeInput("number", "Select Number for Repeated cv", selected = 10, 
+                         choices = c(1,5,10,15)),
+          selectizeInput("repeats", "Select repeats for Repeated cv", selected = 3, 
+                         choices = c(1,2,3,4))
+        ),
+        
+        mainPanel(
+          verbatimTextOutput(outputId = "RegSum2"),
+          verbatimTextOutput(outputId = "IndPrint2"),
+          verbatimTextOutput(outputId = "DepPrint2")
+        )
+      )
+    ),
+      # Fifth tab content
+      tabItem(tabName = "Data",
+              h2("Here's the data"),
+              
+              sidebarPanel(
+                # Button
+                downloadButton("downloadData", "Download"),
+                selectizeInput("team5", "Team", selected = "NOR_TAR",
+                             choices = levels(as.factor(projectData$PitcherTeam))),
+                selectizeInput("pitcher5", "Pitcher", selected = "Bergner, Austin", 
+                             choices = levels(as.factor(projectData$Pitcher))),
+              br()
+              ),
+              
+              mainPanel(
+                DT::dataTableOutput("data")
+              )
+              
+      )
+      
+    )
+  )
+)
+
+
+
+
